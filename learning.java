60df635aa7da3778e62b6bef48851aff2350f98a
V-Adithya <113903821+V-Adithya@users.noreply.github.com>
2023-10-12 03:55:52 +0000

    commit just before the coding began

---
+++ learning.java
@@ -0,0 +1,17 @@
+package com.adroid.cum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View 
+import android.widget.EditText
+import android.widget.Button
+import android.widget.ViewText
+
+public class main_activity extends AppCompatActivity{
+    EditText edt
+    some variables
+    @override
+    public void onCreate(Bundle savedInstanceState){
+        
+    }
+}
