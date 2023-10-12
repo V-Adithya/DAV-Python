49f819d5999c0ff2f56ba738178e721c5343fd62
V-Adithya <113903821+V-Adithya@users.noreply.github.com>
2023-10-12 05:01:48 +0000

    final

--- learning.java
+++ learning.java
@@ -1,18 +1,32 @@
-package com.adroid.cum
+package com.android.learning;
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View 
-import android.widget.EditText
-import android.widget.Button
-import android.widget.ViewText
+import androidx.appcompat.app.AppCompatActivity;
 
 public class learning extends AppCompatActivity{
-    EditText edt
-    some variables
+    /*variables here */
     @override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView()
+        setContentView(R.layout.activity_main)
+        /*
+        You such a whore, ooh
+        I need her, when she on me I don't feel so lonely
+        Don't leave me horny, ride me like a pony
+        Her sweat, her moaning, she's just such a fucking whore
+        I love it
+        I just fuck her on her side
+        And she bitch for me on life
+        For her pussy I would die
+        Every second we on fire
+        Room in private
+        We used the item
+        The 69, she's just such a fuckin'...
+        You such a fucking whore
+        I know you like it more
+        I'll fuck her when I'm bored
+        Like I need, she's a whore
+        I love it
+        The 69, she's just such a fuckin'...
+         */
     }
 }
